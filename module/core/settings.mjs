f1f88a136e2c6cb1cee7d710354097fe1fcda8a5
raysalazar5 <rayblesssalazar@gmail.com>
2026-02-21 13:44:53 -0800

    add print logs to settings.mjs

--- module/core/settings.mjs
+++ module/core/settings.mjs
@@ -122,6 +122,8 @@ export const registerSettingsCore = function(systemManager, rollHandlers, styles
     default: "foundryVTT",
     choices: styleChoices,
     onChange: value => {
+      console.log('changing the style value')
+      console.log(value)
       Utils.switchCSS(value);
       onChangeFunction("style", value);
     }
